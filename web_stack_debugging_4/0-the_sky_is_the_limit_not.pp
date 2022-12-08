fae9078dd5bdc5666393316bff13c063014f220d
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-12-08 12:40:39 +0200

    updated

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # increase the open file limit for nginx processes
 
 exec { 'increase limit':
-  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 5000\"/g" /etc/default/nginx',
   provider => shell,
 }
 
 # restart nginx
 exec { 'restart nginx':
   command  => 'sudo service nginx restart',
-  provider => shell
+  provider => 'shell'
 }

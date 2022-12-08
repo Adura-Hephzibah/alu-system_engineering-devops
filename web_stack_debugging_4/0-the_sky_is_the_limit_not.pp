e4ecbf880a8ca63a5a6c7ae69358dbb0feef141c
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-12-08 12:14:02 +0200

    Sky is the limit, let's bring that limit higher

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# increase the open file limit for nginx processes
+
+exec { 'increase limit'
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ulimit=\"-n 15000\"/g" /etc/default/nginx',
+  provider => shell
+}
+
+# restart nginx
+exec { 'restart nginx':
+  command  => 'sudo service nginx restart',
+  provider => shell
+}

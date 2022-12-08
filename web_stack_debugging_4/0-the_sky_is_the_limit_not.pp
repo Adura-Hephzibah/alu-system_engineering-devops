407b68ef0d50cc31fff49e3e1c538971acc9637b
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-12-08 12:14:53 +0200

    CORRECTED TYPO

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increase the open file limit for nginx processes
 
 exec { 'increase limit'
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ulimit=\"-n 15000\"/g" /etc/default/nginx',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 15000\"/g" /etc/default/nginx',
   provider => shell
 }
 
